93c5dbd9339acc2f715b14dd634ebd2229949e94
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-04-26 21:23:18 +0600

    Style: Sign in Page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,8 +1,42 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, Pressable, TextInput } from 'react-native';
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('email, password', email, password);
+  };
   return (
-    <View>
-      <Text>SignIn</Text>
+    <View className="flex-1 items-center px-12">
+      <View className="mt-20">
+        <Text className="text-center font-MontBold text-3xl">Login</Text>
+        <Text className="text-center font-MontRegular text-xl">To your account</Text>
+      </View>
+      <View className="w-full  flex-1 justify-center gap-4">
+        <View>
+          <Text className="font-MontMedium">Email</Text>
+          <TextInput
+            placeholder="Enter your email"
+            className="w-full rounded-lg border-2 bg-white px-4"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+        </View>
+        <View>
+          <Text className="font-MontMedium">Password</Text>
+          <TextInput
+            placeholder="Enter your password"
+            className="w-full rounded-lg border-2 bg-white px-4"
+            secureTextEntry
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+        </View>
+      </View>
+      <Pressable className="mb-20  w-full items-center justify-center rounded-lg border-2 bg-white px-8 py-3 ">
+        <Text className="font-MontMedium text-xl font-medium">Log In</Text>
+      </Pressable>
     </View>
   );
 }
