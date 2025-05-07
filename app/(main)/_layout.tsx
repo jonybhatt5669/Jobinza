ba5bb6a869320ec15b740dccb05fc3a233fd33d0
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-05-08 02:28:03 +0600

    Fix tabbar issue on position

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,18 +1,33 @@
 import { Tabs } from 'expo-router';
+import { Feather } from '@expo/vector-icons';
 export default function MainLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingTop: 8,
+          borderTopWidth: 2,
+          borderColor: '#000',
+          height: 60,
+        },
+        tabBarShowLabel: false,
+      }}>
       <Tabs.Screen
         name="home"
         options={{
-          title: 'Home',
           headerShown: false,
+          tabBarIcon: ({ color, size }) => <Feather name="home" size={size} color={color} />,
+          tabBarActiveTintColor: '#000',
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           headerShown: false,
+          tabBarIcon: ({ color, size }) => <Feather name="compass" size={size} color={color} />,
+          tabBarActiveTintColor: '#000',
         }}
       />
     </Tabs>
